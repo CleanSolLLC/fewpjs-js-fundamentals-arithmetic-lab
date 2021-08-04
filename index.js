62bbf2954c9bbfc15893071a0bd5a079ad705191
mamark90 <mamark90@gmail.com>
2021-08-04 08:35:49 -0400

    commit tree e1ae1f2a36c92eaf30ecb52a1754793753b52a8d

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1 * num2);
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+let num4 = 25;
+let num3 = 7;
+const mod = Math.round(num4 / num3);
+
+const max = Math.max(20);
